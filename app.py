be63016dc9bd11a86c18fcb9815db5061db04e7e
Prachit Paralikar <127011526+Prachit707@users.noreply.github.com>
2026-02-13 23:10:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-import streamlit as st
-import pandas as pd
+#  import streamlit as st
+# import pandas as pd
 
-st.title("MERX Health IT Opportunities")
+# st.title("MERX Health IT Opportunities")
 
-try:
-    df = pd.read_csv("output.csv")
-    st.success(f"Total Opportunities: {len(df)}")
-    st.dataframe(df, use_container_width=True)
+# try:
+#     df = pd.read_csv("output.csv")
+#     st.success(f"Total Opportunities: {len(df)}")
+#     st.dataframe(df, use_container_width=True)
 
-    st.download_button(
-        label="Download CSV",
-        data=df.to_csv(index=False),
-        file_name="MERX_Output.csv",
-        mime="text/csv"
-    )
+#     st.download_button(
+#         label="Download CSV",
+#         data=df.to_csv(index=False),
+#         file_name="MERX_Output.csv",
+#         mime="text/csv"
+#     )
 
-except:
-    st.error("No data available yet. Run GitHub Action first.")
+# except:
+#     st.error("No data available yet. Run GitHub Action first.")
+
